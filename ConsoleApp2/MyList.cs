19c086b9abbe197803a6777178170c4b32400481
kowalskie789@gma <kowalskie789@gmail.com>
2023-04-16 14:39:35 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp2/MyList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class MyList<T>
+    {
+       private T[] data = new T[3];
+       private int counter = 0;
+        public void InsertData(T x)
+        {
+            
+            if(counter>data.Length-1)
+            {
+
+                T[] newData = new T[counter+1];
+                for (int i = 0; i < data.Length; i++)
+                {
+                    newData[i] = data[i];
+                }
+                data = newData;
+            }
+
+            data[counter++] = x;
+        }
+        public void Print()
+        {
+            for (int i = 0; i < counter; i++)
+            {
+            Console.WriteLine(data[i]);
+            }
+        }
+    
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+/*
+//EXERCISE 6
+Assume that C# don’t have generic list.
+Implement generic MyClass list based on array.
+*/
+
+using ConsoleApp2;
+
+MyList<int> lista = new MyList<int>();
+lista.InsertData(3);
+lista.InsertData(5);
+lista.InsertData(6);
+lista.InsertData(7);
+lista.Print();
+
+MyList<string> lista2 = new MyList<string>();
+lista2.InsertData("ELO");
+lista2.InsertData("CODECOOLERZY");
+lista2.InsertData("!!!!!!!!!!!!!!!");
+lista2.InsertData("!!!!!!!!!!!!!");
+lista2.InsertData("!!!!!!!!!!!!");
+lista2.InsertData("!!!!!!!!!!!");
+lista2.InsertData("!!!!!!!!!!");
+lista2.InsertData("!!!!!!!!!");
+lista2.InsertData("!!!!!!!!");
+lista2.InsertData("!!!!!!!");
+lista2.InsertData("!!!!!!");
+lista2.InsertData("!!!!!");
+lista2.InsertData("!!!!");
+lista2.InsertData("!!!");
+lista2.InsertData("!!");
+lista2.InsertData("!");
+lista2.Print();
